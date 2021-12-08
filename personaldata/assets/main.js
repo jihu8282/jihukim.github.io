41f44f530fb1813d3121f1ebde515f246f0f8873
Jihu Kim <47605642+jihu8282@users.noreply.github.com>
2021-12-08 00:27:04 -0500

    without blob

--- personaldata/assets/main.js
+++ personaldata/assets/main.js
@@ -12,54 +12,54 @@ console.log(attribute);
 console.log(wordEl);
 if(attribute === "dog_lover"){
   wordEl.innerText = 'dog lover';
-  descriptEl.innerText = "She cares about animals a lot";
+  descriptEl.innerText = ": She cares about animals a lot";
   whiteRecEl.classList.add("pos2");
   whiteRec2El.classList.add("pos9");
 
 }else if(attribute === "full_of_personality"){
    wordEl.innerText = 'full of personality';
-  descriptEl.innerText = "I personally enjoy following her outfits on insta";
+  descriptEl.innerText = ": I personally enjoy following her outfits on insta";
   whiteRecEl.classList.add("pos1");
   whiteRec2El.classList.add("pos2");
 }else if(attribute === "talented_dancer"){
   wordEl.innerText = 'talented dancer';
-  descriptEl.innerText = "I love dancing with her!";
+  descriptEl.innerText = ": I love dancing with her!";
   whiteRecEl.classList.add("pos2");
   whiteRec2El.classList.add("pos3");
 }else if(attribute === "lovable"){
   wordEl.innerText = 'lovable';
-  descriptEl.innerText = "She's best at smiling with her eyes";
+  descriptEl.innerText = ": She's best at smiling with her eyes";
   whiteRecEl.classList.add("pos8");
   whiteRec2El.classList.add("pos3");
 }else if(attribute === "shy"){
   wordEl.innerText = 'shy';
-  descriptEl.innerText = "It took a while for her to open up";
+  descriptEl.innerText = ": It took a while for her to open up";
   whiteRecEl.classList.add("pos7");
   whiteRec2El.classList.add("pos4");
   
 }else if(attribute === "cute"){
   wordEl.innerText = 'cute';
-  descriptEl.innerText = "adorable!";
+  descriptEl.innerText = ": adorable!";
   whiteRecEl.classList.add("pos5");
   whiteRec2El.classList.add("pos1");
 }else if(attribute === "too_funny"){
   wordEl.innerText = 'too funny';
-  descriptEl.innerText = "one of the funniest nights when we all hung out together for my birthday";
+  descriptEl.innerText = ": One of the funniest nights when we all hung out together for my birthday";
   whiteRecEl.classList.add("pos6");
   whiteRec2El.classList.add("pos8");
 }else if(attribute === "thoughtful"){
   wordEl.innerText = 'thoughtful';
-  descriptEl.innerText = "the reason I got through college";
+  descriptEl.innerText = ": She's the reason I got through college";
   whiteRecEl.classList.add("pos9");
   whiteRec2El.classList.add("pos8");
 }else if(attribute === "warm_hearted"){
   wordEl.innerText = 'warm-hearted';
-  descriptEl.innerText = "she was my go-to person whenever I needed someone to talk to";
+  descriptEl.innerText = ": She was my go-to person whenever I needed someone to talk to";
   whiteRecEl.classList.add("pos8");
   whiteRec2El.classList.add("pos6");
 }else if(attribute === "small_but_strong"){
   wordEl.innerText = 'small but strong';
-  descriptEl.innerText = "she is stronger than she looks";
+  descriptEl.innerText = ": She is stronger than she looks";
   whiteRecEl.classList.add("pos8");
   whiteRec2El.classList.add("pos5");
 }
